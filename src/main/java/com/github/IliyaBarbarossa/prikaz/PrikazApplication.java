4823378555acafeff4ff4b720f3e7443c6114198
Илья <iliya168@yandex.ru>
2024-07-23 18:36:54 +0300

    готово бд с приказами CRUD ссоединено с базой пользователей с проверкой уровня доступа

---
+++ src/main/java/com/github/IliyaBarbarossa/prikaz/PrikazApplication.java
@@ -0,0 +1,13 @@
+package com.github.IliyaBarbarossa.prikaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrikazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrikazApplication.class, args);
+	}
+
+}
